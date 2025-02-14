4cd324582d477ff39115198db4cc32c801192f82
shahidul shohid <shahidulislamshohid7@gmail.com>
2025-02-14 19:39:12 +0600

    first commit

---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,10 @@
+
+const MainLayout = () => {
+    return (
+        <div>
+            <h1>Main layout</h1>
+        </div>
+    );
+};
+
+export default MainLayout;
---
+++ src/routes/Routers.jsx
@@ -0,0 +1,10 @@
+
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layout/MainLayout";
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainLayout></MainLayout>,
+    },
+  ]);
+  export default router
