ca019d5f35b5a0451cf5446d785f3a6914060fa7
shahidul shohid <shahidulislamshohid7@gmail.com>
2025-02-15 00:49:15 +0600

    .en.local file create is done for keeping variable key

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,10 +1,12 @@
 import React, { createContext, useState } from 'react';
+import app from '../firebase/firebase.config';
 
 export const authContext = createContext()
+const auth = getAuth(app)
 const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null)
     const [loading, setLoading] = useState(true)
-    
+
     const authInfo = {
         user, 
     }
