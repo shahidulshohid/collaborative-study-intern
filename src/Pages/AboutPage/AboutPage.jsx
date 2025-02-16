0cebba96375a27005158cbd62c98bdfb57fca388
shahidul shohid <shahidulislamshohid7@gmail.com>
2025-02-16 12:19:35 +0600

    code .

--- src/Pages/AboutPage/AboutPage.jsx
+++ src/Pages/AboutPage/AboutPage.jsx
@@ -1,11 +1,16 @@
-
-
 const AboutPage = () => {
-    return (
-        <div>
-            <h3>About page</h3>
-        </div>
-    );
+    let dict = {};
+    for (let i = 0; i < n; i++) {
+        const arrItem = readLine().split(' ');
+        const key = arrItem[0];
+        const value = parseInt(arrItem[1], 10);
+        dict[key] = value;
+    }
+  return (
+    <div>
+      <h3>About page</h3>
+    </div>
+  );
 };
 
 export default AboutPage;
