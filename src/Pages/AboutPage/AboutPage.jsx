a970f2e34d7ad230196401a094b0c2bce01c016b
shahidul shohid <shahidulislamshohid7@gmail.com>
2025-02-16 16:35:14 +0600

    some update

--- src/Pages/AboutPage/AboutPage.jsx
+++ src/Pages/AboutPage/AboutPage.jsx
@@ -1,9 +1,17 @@
 const AboutPage = () => {
  const maxNumber = (arr) => {
-    const max = Math.max(...arr)
-    console.log(max)
+    // const max = Math.max(...arr)
+    // console.log(max)
+    let max = arr[0]
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] > max){
+            max = arr[i]
+        }
+        console.log(max)
+    }
  }
- maxNumber([-1, -2, -3, -4])
+//  maxNumber([-1, -2, -3, -4])
+maxNumber([1,2,3,4,5])
 
   return (
     <div>
